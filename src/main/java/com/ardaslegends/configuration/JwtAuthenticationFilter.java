f506fa6f1f7cbe83658e04ee01a12fe9bd573a28
Alexandros Stergiou <jutaroroche@gmail.com>
2024-09-17 15:25:55 +0200

    Added blocking of non-authenticated requests. Added handling of newly registered players too to not return an error even if the user is not found in the DB

--- src/main/java/com/ardaslegends/configuration/JwtAuthenticationFilter.java
+++ src/main/java/com/ardaslegends/configuration/JwtAuthenticationFilter.java
@@ -1,5 +1,6 @@
 package com.ardaslegends.configuration;
 
+import com.ardaslegends.presentation.api.PlayerRestController;
 import com.ardaslegends.service.authentication.JwtUtil;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
@@ -7,27 +8,34 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.lang.NonNull;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
+import org.springframework.web.servlet.HandlerExceptionResolver;
 
 import java.io.IOException;
 
 @Slf4j
 @Component
 public class JwtAuthenticationFilter extends OncePerRequestFilter {
-    //private final HandlerExceptionResolver handlerExceptionResolver;
+    private final HandlerExceptionResolver handlerExceptionResolver;
 
     private final JwtUtil jwtUtil;
-    //private final UserDetailsService userDetailsService;
+    private final UserDetailsService userDetailsService;
 
     public JwtAuthenticationFilter(
-            JwtUtil jwtUtil
-            //UserDetailsService userDetailsService,
-            //HandlerExceptionResolver handlerExceptionResolver
+            JwtUtil jwtUtil,
+            UserDetailsService userDetailsService,
+            HandlerExceptionResolver handlerExceptionResolver
     ) {
         this.jwtUtil = jwtUtil;
-        //this.userDetailsService = userDetailsService;
-        //this.handlerExceptionResolver = handlerExceptionResolver;
+        this.userDetailsService = userDetailsService;
+        this.handlerExceptionResolver = handlerExceptionResolver;
     }
 
     @Override
@@ -51,24 +59,39 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
             final String userDiscordId = jwtUtil.extractDiscordIdFromJWT(jwt);
             log.debug("User Discord ID: {}", userDiscordId);
 
-            //Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+            Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
 
-            //if (userDiscordId != null && authentication == null) {
-            if (userDiscordId != null) {
-                //UserDetails userDetails = this.userDetailsService.loadUserByUsername(userDiscordId);
+            if (userDiscordId != null && authentication == null) {
+                //if (userDiscordId != null) {
                 try {
                     boolean isTokenValid = jwtUtil.isTokenValid(jwt);
                     log.debug("Token is valid: {}", isTokenValid);
-                    //if (jwtUtil.isTokenValid(jwt)) {
-                    // UsernamePasswordAuthenticationToken authToken = new UsernamePasswordAuthenticationToken(
-                    //       userDetails,
-                    //     null,
-                    //   userDetails.getAuthorities()
-                    //);
-
-                    //authToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
-                    //SecurityContextHolder.getContext().setAuthentication(authToken);
-                    //}
+                    if (jwtUtil.isTokenValid(jwt)) {
+                        try {
+                            log.debug("Loading user by username to get UserDetails");
+                            // load user by username to get UserDetails
+                            UserDetails userDetails = this.userDetailsService.loadUserByUsername(userDiscordId);
+                            UsernamePasswordAuthenticationToken authToken = new UsernamePasswordAuthenticationToken(
+                                    userDetails,
+                                    null,
+                                    userDetails.getAuthorities()
+                            );
+                            authToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+                            SecurityContextHolder.getContext().setAuthentication(authToken);
+                        } catch (Exception e) {
+                            // if user does not exist and we are in the post player endpoint, we can create a new user
+                            // instead of throwing an exception
+                            // if we are not in the /register endpoint, we should throw an exception
+                            if (request.getRequestURI().equals(PlayerRestController.BASE_URL) && request.getMethod().equals("POST")) {
+                                log.info("User does not exist, but we are in the /register endpoint, so we will create a new user");
+                                filterChain.doFilter(request, response);
+                            } else {
+                                log.error("Error setting user authentication in security context", e);
+                                handlerExceptionResolver.resolveException(request, response, null, e);
+                                return;
+                            }
+                        }
+                    }
                 } catch (Exception e) {
                     log.debug("Token is invalid", e);
                     response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
--- src/main/java/com/ardaslegends/configuration/SecurityConfiguration.java
+++ src/main/java/com/ardaslegends/configuration/SecurityConfiguration.java
@@ -27,7 +27,7 @@ public class SecurityConfiguration {
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http.csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/**").permitAll()
+                        .requestMatchers("/auth/**").permitAll()
                         .anyRequest().authenticated()
                 )
                 .authenticationProvider(authenticationProvider)
