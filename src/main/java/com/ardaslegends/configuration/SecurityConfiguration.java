d79299ffe5711f0e3a86a0ac89f5859d12355552
Alexandros Stergiou <jutaroroche@gmail.com>
2024-09-18 10:50:05 +0200

    Opened swagger route to everyone

--- src/main/java/com/ardaslegends/configuration/SecurityConfiguration.java
+++ src/main/java/com/ardaslegends/configuration/SecurityConfiguration.java
@@ -28,6 +28,7 @@ public class SecurityConfiguration {
         http.csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/auth/**").permitAll()
+                        .requestMatchers("/swagger-ui/**", "/v3/api-docs/**").permitAll()
                         .requestMatchers("/api/player/**").hasRole("USER")
                         .anyRequest().authenticated()
                 )
