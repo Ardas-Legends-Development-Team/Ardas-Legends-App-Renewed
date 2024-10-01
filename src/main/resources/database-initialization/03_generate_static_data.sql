026417d3402b4223c1985f9c637c4b5f164ca85a
Alexandros Stergiou <jutaroroche@gmail.com>
2024-10-01 11:31:11 +0200

    Static data: added what units are available for each faction

--- src/main/resources/database-initialization/03_generate_static_data.sql
+++ src/main/resources/database-initialization/03_generate_static_data.sql
@@ -4338,7 +4338,6 @@ begin
     INSERT INTO unit_types(unit_name, token_cost, is_mounted) VALUES ('Angmar Orc', 4, false);
     INSERT INTO unit_types(unit_name, token_cost, is_mounted) VALUES ('Dol Guldur Orc', 4, false);
     INSERT INTO unit_types(unit_name, token_cost, is_mounted) VALUES ('Levymen', 4, false);
-    INSERT INTO unit_types(unit_name, token_cost, is_mounted) VALUES ('Clansmen', 4, false);
     INSERT INTO unit_types(unit_name, token_cost, is_mounted) VALUES ('Nomad Guard', 4, false);
     INSERT INTO unit_types(unit_name, token_cost, is_mounted) VALUES ('Bree Guard', 4, false);
     INSERT INTO unit_types(unit_name, token_cost, is_mounted) VALUES ('Gundabad Orc Archers', 4, false);
@@ -4434,6 +4433,7 @@ begin
     INSERT INTO unit_types(unit_name, token_cost, is_mounted) VALUES ('Gondor Archers', 8, false);
     INSERT INTO unit_types(unit_name, token_cost, is_mounted) VALUES ('Umbar Archers', 8, false);
     INSERT INTO unit_types(unit_name, token_cost, is_mounted) VALUES ('Lamedon Archers', 8, false);
+    INSERT INTO unit_types(unit_name, token_cost, is_mounted) VALUES ('Dalish Archer', 8, false);
     INSERT INTO unit_types(unit_name, token_cost, is_mounted) VALUES ('Wood Elf Scouts', 8, false);
     INSERT INTO unit_types(unit_name, token_cost, is_mounted) VALUES ('Dunlending Berserkers', 8, false);
     INSERT INTO unit_types(unit_name, token_cost, is_mounted) VALUES ('Uruk Berserkers', 8, false);
@@ -4519,9 +4519,227 @@ begin
     INSERT INTO unit_types(unit_name, token_cost, is_mounted) VALUES ('Two-Headed Ologs', 90, false);
 
 
--- Add units to their corresponding faction
-    INSERT INTO factions_units (unit_name, faction_id) VALUES ('Swan Knight', 1);
+    -- Add units to their corresponding faction
+-- Angmar (Faction ID: 1)
+    INSERT INTO factions_units (unit_name, faction_id) VALUES ('Angmar Wargs', 1);
+    INSERT INTO factions_units (unit_name, faction_id) VALUES ('Angmar Orc', 1);
+    INSERT INTO factions_units (unit_name, faction_id) VALUES ('Angmar Orc Archers', 1);
+    INSERT INTO factions_units (unit_name, faction_id) VALUES ('Angmar Banner Bearer', 1);
+    INSERT INTO factions_units (unit_name, faction_id) VALUES ('Angmar Troll', 1);
+    INSERT INTO factions_units (unit_name, faction_id) VALUES ('Two-headed Angmar Trolls', 1);
+    INSERT INTO factions_units (unit_name, faction_id) VALUES ('Hillmen', 1);
+    INSERT INTO factions_units (unit_name, faction_id) VALUES ('Rhudaur Axethrowers', 1);
+    INSERT INTO factions_units (unit_name, faction_id) VALUES ('Rhudaur Warriors', 1);
+    INSERT INTO factions_units (unit_name, faction_id) VALUES ('Rhudaur Banner Bearer', 1);
+    INSERT INTO factions_units (unit_name, faction_id) VALUES ('Hill Trolls', 1);
 
+
+-- Bree (Faction ID: 2)
+    INSERT INTO factions_units (unit_name, faction_id) VALUES ('Bree Guard', 2);
+
+-- Dale (Faction ID: 3)
+    INSERT INTO factions_units (unit_name, faction_id) VALUES ('Dalish Soldier', 3);
+    INSERT INTO factions_units (unit_name, faction_id) VALUES ('Dalish Archer', 3);
+    INSERT INTO factions_units (unit_name, faction_id) VALUES ('Dalish Banner Bearer', 3);
+
+-- Dol Amroth (Faction ID: 4)
+    INSERT INTO factions_units (unit_name, faction_id) VALUES ('Dol Amroth Men-at-arms', 4);
+    INSERT INTO factions_units (unit_name, faction_id) VALUES ('Dol Amroth Men-at-arms Cavalry', 4);
+    INSERT INTO factions_units (unit_name, faction_id) VALUES ('Dol Amroth Banner Bearer', 4);
+    INSERT INTO factions_units (unit_name, faction_id) VALUES ('Swan Knight', 4);
+    INSERT INTO factions_units (unit_name, faction_id) VALUES ('Mounted Swan Knight', 4);
+
+-- Dol Guldur (Faction ID: 5)
+    INSERT INTO factions_units (unit_name, faction_id) VALUES ('Dol Guldur Orc', 5);
+    INSERT INTO factions_units (unit_name, faction_id) VALUES ('Dol Guldur Orc Archers', 5);
+    INSERT INTO factions_units (unit_name, faction_id) VALUES ('Dol Guldur Banner Bearer', 5);
+    INSERT INTO factions_units (unit_name, faction_id) VALUES ('Mirkwood Spiders', 5);
+    INSERT INTO factions_units (unit_name, faction_id) VALUES ('Mordor Spiders', 5);
+    INSERT INTO factions_units (unit_name, faction_id) VALUES ('Mirkwood Spider Archers', 5);
+    INSERT INTO factions_units (unit_name, faction_id) VALUES ('Mirk Trolls', 5);
+
+
+-- Dorwinion (Faction ID: 6)
+    INSERT INTO factions_units (unit_name, faction_id) VALUES ('Vintner Guard', 6);
+    INSERT INTO factions_units (unit_name, faction_id) VALUES ('Vintner Guard Crossbowmen', 6);
+    INSERT INTO factions_units (unit_name, faction_id) VALUES ('Dorwinion Banner Bearer', 6);
+    INSERT INTO factions_units (unit_name, faction_id) VALUES ('Bladorthin Warriors', 6);
+    INSERT INTO factions_units (unit_name, faction_id) VALUES ('Bladorthin Archers', 6);
+
+-- Dunland (Faction ID: 7)
+    INSERT INTO factions_units (unit_name, faction_id) VALUES ('Dunlending Axethrowers', 7);
+    INSERT INTO factions_units (unit_name, faction_id) VALUES ('Dunlending Warriors', 7);
+    INSERT INTO factions_units (unit_name, faction_id) VALUES ('Dunlending Bowmen', 7);
+    INSERT INTO factions_units (unit_name, faction_id) VALUES ('Dunlending Banner Bearer', 7);
+    INSERT INTO factions_units (unit_name, faction_id) VALUES ('Dunlending Berserkers', 7);
+
+
+-- Durin's Folk (Faction ID: 8)
+    INSERT INTO factions_units (unit_name, faction_id) VALUES ('Dwarf Warriors', 8);
+    INSERT INTO factions_units (unit_name, faction_id) VALUES ('Dwarf Axe Throwers', 8);
+    INSERT INTO factions_units (unit_name, faction_id) VALUES ('Dwarven Boar Riders', 8);
+    INSERT INTO factions_units (unit_name, faction_id) VALUES ('Dwarf Banner Bearer', 8);
+    INSERT INTO factions_units (unit_name, faction_id) VALUES ('Dwarven Axe Thrower Boar Riders', 8);
+
+
+-- Ered Luin (Faction ID: 9)
+    INSERT INTO factions_units (unit_name, faction_id) VALUES ('Dwarf Warriors', 8);
+    INSERT INTO factions_units (unit_name, faction_id) VALUES ('Dwarf Axe Throwers', 8);
+    INSERT INTO factions_units (unit_name, faction_id) VALUES ('Dwarven Boar Riders', 8);
+    INSERT INTO factions_units (unit_name, faction_id) VALUES ('Blue Mountains Banner Bearer', 9);
+    INSERT INTO factions_units (unit_name, faction_id) VALUES ('Dwarven Axe Thrower Boar Riders', 9);
+
+
+-- Gondor (Faction ID: 10)
+    INSERT INTO factions_units (unit_name, faction_id) VALUES ('Gondor Soldier', 10);
+    INSERT INTO factions_units (unit_name, faction_id) VALUES ('Gondor Archers', 10);
+    INSERT INTO factions_units (unit_name, faction_id) VALUES ('Gondor Cavalrymen', 10);
+    INSERT INTO factions_units (unit_name, faction_id) VALUES ('Gondor Banner Bearer', 10);
+    INSERT INTO factions_units (unit_name, faction_id) VALUES ('Gondor Tower Guard', 10);
+    INSERT INTO factions_units (unit_name, faction_id) VALUES ('Lamedon Warrior', 10);
+    INSERT INTO factions_units (unit_name, faction_id) VALUES ('Lamedon Horseman', 10);
+    INSERT INTO factions_units (unit_name, faction_id) VALUES ('Lamedon Banner Bearer', 10);
+    INSERT INTO factions_units (unit_name, faction_id) VALUES ('Pelargir Marine', 10);
+    INSERT INTO factions_units (unit_name, faction_id) VALUES ('Pelargir Banner Bearer', 10);
+    INSERT INTO factions_units (unit_name, faction_id) VALUES ('Pinnath Gelin Soldiers', 10);
+    INSERT INTO factions_units (unit_name, faction_id) VALUES ('Pinnath Gelin Cavalry', 10);
+    INSERT INTO factions_units (unit_name, faction_id) VALUES ('Pinnath Gelin Banner Bearer', 10);
+    INSERT INTO factions_units (unit_name, faction_id) VALUES ('Levymen', 10);
+    INSERT INTO factions_units (unit_name, faction_id) VALUES ('Ithilien Rangers', 10);
+    INSERT INTO factions_units (unit_name, faction_id) VALUES ('Ithilien Banner Bearer', 10);
+    INSERT INTO factions_units (unit_name, faction_id) VALUES ('Blackroot Vale Soldiers', 10);
+    INSERT INTO factions_units (unit_name, faction_id) VALUES ('Blackroot Vale Bowmen', 10);
+    INSERT INTO factions_units (unit_name, faction_id) VALUES ('Blackroot Vale Cavalry', 10);
+    INSERT INTO factions_units (unit_name, faction_id) VALUES ('Blackroot Vale Banner Bearer', 10);
+
+
+-- Gulf of Harad (Faction ID: 11)
+    INSERT INTO factions_units (unit_name, faction_id) VALUES ('Gulfing Warrior', 11);
+    INSERT INTO factions_units (unit_name, faction_id) VALUES ('Gulfing Archer', 11);
+    INSERT INTO factions_units (unit_name, faction_id) VALUES ('Gulfing Banner Bearer', 11);
+
+-- Gundabad (Faction ID: 12)
+    INSERT INTO factions_units (unit_name, faction_id) VALUES ('Gundabad Orc', 12);
+    INSERT INTO factions_units (unit_name, faction_id) VALUES ('Gundabad Wargs', 12);
+    INSERT INTO factions_units (unit_name, faction_id) VALUES ('Gundabad Warg Riders', 12);
+    INSERT INTO factions_units (unit_name, faction_id) VALUES ('Gundabad Warg Rider Archers', 12);
+    INSERT INTO factions_units (unit_name, faction_id) VALUES ('Gundabad Orc Archers', 12);
+    INSERT INTO factions_units (unit_name, faction_id) VALUES ('Gundabad Banner Bearer', 12);
+    INSERT INTO factions_units (unit_name, faction_id) VALUES ('Uruks', 12);
+
+-- Half-Trolls (Faction ID: 13)
+    INSERT INTO factions_units (unit_name, faction_id) VALUES ('Half-Trolls', 13);
+    INSERT INTO factions_units (unit_name, faction_id) VALUES ('Half-Troll Warrior', 13);
+    INSERT INTO factions_units (unit_name, faction_id) VALUES ('Half-Troll Rhino Riders', 13);
+    INSERT INTO factions_units (unit_name, faction_id) VALUES ('Half Troll Banner Bearer', 13);
+
+-- Harnennor (Faction ID: 14)
+    INSERT INTO factions_units (unit_name, faction_id) VALUES ('Harnenor Soldier', 14);
+    INSERT INTO factions_units (unit_name, faction_id) VALUES ('Harnenor Archer', 14);
+    INSERT INTO factions_units (unit_name, faction_id) VALUES ('Harnennor Warrior', 14);
+    INSERT INTO factions_units (unit_name, faction_id) VALUES ('Harnennor Banner Bearer', 14);
+
+-- Hobbits (Faction ID: 15)
+    INSERT INTO factions_units (unit_name, faction_id) VALUES ('Hobbit Bounder', 15);
+    INSERT INTO factions_units (unit_name, faction_id) VALUES ('Mounted Hobbit Bounder', 15);
+    INSERT INTO factions_units (unit_name, faction_id) VALUES ('Huorn', 15);
+
+
+-- Isengard (Faction ID: 16)
+    INSERT INTO factions_units (unit_name, faction_id) VALUES ('Isengard Wargs', 16);
+    INSERT INTO factions_units (unit_name, faction_id) VALUES ('Isengard Snaga', 16);
+    INSERT INTO factions_units (unit_name, faction_id) VALUES ('Isengard Snaga Warg Riders', 16);
+    INSERT INTO factions_units (unit_name, faction_id) VALUES ('Isengard Snaga Warg Rider Archers', 16);
+    INSERT INTO factions_units (unit_name, faction_id) VALUES ('Uruks', 16);
+    INSERT INTO factions_units (unit_name, faction_id) VALUES ('Snaga Orc Archers', 16);
+    INSERT INTO factions_units (unit_name, faction_id) VALUES ('Uruk Berserkers', 16);
+
+
+-- Lindon (Faction ID: 17)
+    INSERT INTO factions_units (unit_name, faction_id) VALUES ('Lindon Cavalry', 17);
+    INSERT INTO factions_units (unit_name, faction_id) VALUES ('Lindon Banner Bearer', 17);
+    INSERT INTO factions_units (unit_name, faction_id) VALUES ('Elf Warriors', 17);
+
+
+-- Lothlórien (Faction ID: 18)
+    INSERT INTO factions_units (unit_name, faction_id) VALUES ('Galadhrim Banner Bearer', 18);
+    INSERT INTO factions_units (unit_name, faction_id) VALUES ('Galadhrim Cavalry', 18);
+    INSERT INTO factions_units (unit_name, faction_id) VALUES ('Elf Warriors', 18);
+
+
+-- Mordor (Faction ID: 19)
+    INSERT INTO factions_units (unit_name, faction_id) VALUES ('Mordor Wargs', 19);
+    INSERT INTO factions_units (unit_name, faction_id) VALUES ('Mordor Orc', 19);
+    INSERT INTO factions_units (unit_name, faction_id) VALUES ('Mordor Orc Archers', 19);
+    INSERT INTO factions_units (unit_name, faction_id) VALUES ('Mordor Spider Archers', 19);
+    INSERT INTO factions_units (unit_name, faction_id) VALUES ('Mordor Banner Bearer', 19);
+    INSERT INTO factions_units (unit_name, faction_id) VALUES ('Black Uruks', 19);
+    INSERT INTO factions_units (unit_name, faction_id) VALUES ('Black Uruk Archer', 19);
+    INSERT INTO factions_units (unit_name, faction_id) VALUES ('Black Uruk Banner Bearer', 19);
+    INSERT INTO factions_units (unit_name, faction_id) VALUES ('Ologs', 19);
+    INSERT INTO factions_units (unit_name, faction_id) VALUES ('Two-Headed Ologs', 19);
+
+-- Morwaith (Faction ID: 20)
+    INSERT INTO factions_units (unit_name, faction_id) VALUES ('Morwaith Warrior', 20);
+    INSERT INTO factions_units (unit_name, faction_id) VALUES ('Morwaith Zebra Rider', 20);
+    INSERT INTO factions_units (unit_name, faction_id) VALUES ('Morwaith Banner Bearer', 20);
+
+-- Nomads (Faction ID: 21)
+    INSERT INTO factions_units (unit_name, faction_id) VALUES ('Nomad Guard', 21);
+    INSERT INTO factions_units (unit_name, faction_id) VALUES ('Nomad Banner Bearer', 21);
+
+-- Rangers of the North (Faction ID: 22)
+    INSERT INTO factions_units (unit_name, faction_id) VALUES ('Rangers', 22);
+    INSERT INTO factions_units (unit_name, faction_id) VALUES ('Ranger Banner Bearer', 22);
+    INSERT INTO factions_units (unit_name, faction_id) VALUES ('Ranger Outrider', 22);
+
+-- Rhúdel (Faction ID: 23)
+    INSERT INTO factions_units (unit_name, faction_id) VALUES ('Easterling Clansman', 23);
+    INSERT INTO factions_units (unit_name, faction_id) VALUES ('Easterling Warrior', 23);
+    INSERT INTO factions_units (unit_name, faction_id) VALUES ('Easterling Fire-thrower', 23);
+    INSERT INTO factions_units (unit_name, faction_id) VALUES ('Easterling Archers', 23);
+    INSERT INTO factions_units (unit_name, faction_id) VALUES ('Easterling Horseman', 23);
+    INSERT INTO factions_units (unit_name, faction_id) VALUES ('Easterling Banner Bearer', 23);
+    INSERT INTO factions_units (unit_name, faction_id) VALUES ('Golden Easterling', 23);
+    INSERT INTO factions_units (unit_name, faction_id) VALUES ('Mounted Golden Easterlings', 23);
+
+
+-- Rivendell (Faction ID: 24)
+    INSERT INTO factions_units (unit_name, faction_id) VALUES ('Rivendell Cavalry', 24);
+    INSERT INTO factions_units (unit_name, faction_id) VALUES ('Rivendell Banner Bearer', 24);
+    INSERT INTO factions_units (unit_name, faction_id) VALUES ('Elf Warriors', 24);
+
+
+-- Rohan (Faction ID: 25)
+    INSERT INTO factions_units (unit_name, faction_id) VALUES ('Rohirrim Warriors', 25);
+    INSERT INTO factions_units (unit_name, faction_id) VALUES ('Rohirrim Archers', 25);
+    INSERT INTO factions_units (unit_name, faction_id) VALUES ('Rohirrim Cavalry', 25);
+    INSERT INTO factions_units (unit_name, faction_id) VALUES ('Rohirrim Banner Bearer', 25);
+
+-- Southron Coast (Faction ID: 26)
+    INSERT INTO factions_units (unit_name, faction_id) VALUES ('Southron Coast Warrior', 26);
+    INSERT INTO factions_units (unit_name, faction_id) VALUES ('Southron Coast Archers', 26);
+    INSERT INTO factions_units (unit_name, faction_id) VALUES ('Coast Southron Banner Bearer', 26);
+
+-- Taurethrim (Faction ID: 27)
+    INSERT INTO factions_units (unit_name, faction_id) VALUES ('Taurethrim Warrior', 27);
+    INSERT INTO factions_units (unit_name, faction_id) VALUES ('Taurethrim Blowgunners', 27);
+    INSERT INTO factions_units (unit_name, faction_id) VALUES ('Taurethrim Banner Bearer', 27);
+
+-- Umbar (Faction ID: 28)
+    INSERT INTO factions_units (unit_name, faction_id) VALUES ('Umbar Corsairs', 28);
+    INSERT INTO factions_units (unit_name, faction_id) VALUES ('Umbar Soldiers', 28);
+    INSERT INTO factions_units (unit_name, faction_id) VALUES ('Umbar Archers', 28);
+    INSERT INTO factions_units (unit_name, faction_id) VALUES ('Umbar Cavalrymen', 28);
+    INSERT INTO factions_units (unit_name, faction_id) VALUES ('Umbar Banner Bearer', 28);
+    INSERT INTO factions_units (unit_name, faction_id) VALUES ('Gondorian Renegades', 28);
+
+
+-- Woodland Realm (Faction ID: 29)
+    INSERT INTO factions_units (unit_name, faction_id) VALUES ('Wood-Elf Scouts', 29);
+    INSERT INTO factions_units (unit_name, faction_id) VALUES ('Wood-Elf Elk Rider', 29);
+    INSERT INTO factions_units (unit_name, faction_id) VALUES ('Wood-Elf Warriors', 18);
+    INSERT INTO factions_units (unit_name, faction_id) VALUES ('Wood-Elf Banner Bearer', 18);
 
 end;
 $BODY$;
